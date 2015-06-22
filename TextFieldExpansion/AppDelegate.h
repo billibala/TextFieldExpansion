8f0c8dd71703ef4d041b96989b654dd33b5f9db0
Bill So <hokchun@gmail.com>
2015-06-21 17:57:55 -0700

    Initial Commit

---
+++ TextFieldExpansion/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TextFieldExpansion
+//
+//  Created by Bill So on 6/21/15.
+//  Copyright © 2015 Bill So. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TextFieldExpansion/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  TextFieldExpansion
+//
+//  Created by Bill So on 6/21/15.
+//  Copyright © 2015 Bill So. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+@end
